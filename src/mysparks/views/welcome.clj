5167f4335e5825d15bd79327054920e8ad0ca3e9
Michael Cramm <gmcramm@gmail.com>
2012-09-11 15:34:30 -0600

    Render sparks in full/conensed mode

--- src/mysparks/views/welcome.clj
+++ src/mysparks/views/welcome.clj
@@ -4,8 +4,23 @@
   (:use [noir.core :only [defpage]]
         [hiccup.core :only [html]]))
 
+(def example-sparks
+  [
+   {:content "This is the first spark I created. It is just a test."
+    :created "2012-09-11 11:05:32"
+    :additions [
+                {:content "This is an addition to the spark. Sparks only be added to."
+                 :created "2012-09-11 13:03:45"}
+                {:content "Yet another addition"
+                 :created "2012-09-11 14:17:62"}]}
+   {:content "This is the second spark I created. Yes, it is another test."
+    :created "2012-09-11 11:05:32"
+    :additions [
+                {:content "This spark only has one addition to it."
+                 :created "2012-09-11 14:17:62"}]}
+   ])
 (defpage "/" []
-         (templates/root))
+         (templates/root example-sparks))
 
 (defpage "/read" []
-         (templates/read-sparks))
+         (templates/read-sparks example-sparks))
